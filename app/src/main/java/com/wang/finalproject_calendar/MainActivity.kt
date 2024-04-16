ddc2766ed4576013ee5649e5686fad356d9e2a14
Mariya Zolovska <2230965@champlaincollege.qc.ca>
2024-04-15 21:44:38 -0400

    created a comment

--- app/src/main/java/com/wang/finalproject_calendar/MainActivity.kt
+++ app/src/main/java/com/wang/finalproject_calendar/MainActivity.kt
@@ -2,7 +2,7 @@ package com.wang.finalproject_calendar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//mariyas comment
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
